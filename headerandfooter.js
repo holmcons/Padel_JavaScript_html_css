dc3a3a8cbaff05bcceb3800da2357e532e2bf890
Johan Holm <johan_holm@live.com>
2022-12-06 14:14:46 +0100

    Fixed footer with responsivity

--- headerandfooter.js
+++ headerandfooter.js
@@ -15,9 +15,30 @@ class TheHeader extends HTMLElement {
                     <li><a href="#">Varuautomat</a></li>
                 </ul>
             </nav>
-            <a class="cta" href="#"><button>Kontakt</button></a>
         </header>
         `
     }
 }
 customElements.define('the-header', TheHeader)
+
+//js kod för att köra footer
+class TheFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <ul>
+                <li>Kisa Padelcenter</li>
+                <li>Sörbäcksvägen 5</li>
+                <li>SE-590 38 KISA</li>
+            </ul>
+            <ul>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Privacy Policy</a></li>
+            </ul>
+            <a class="cta" href="#"><button>Kontakt</button></a>
+        </footer>
+        `
+    }
+}
+customElements.define('the-footer', TheFooter)
